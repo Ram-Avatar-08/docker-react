d6c3133808e3d56ac16ed87107db0f1f3b8064b0
root <root@Ramavatar.localdomain>
2025-08-12 13:23:32 +0000

    git-action for CI

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
-
-      
-          Docker image of React type ho rha header
+        Technology used are:
+        <ol>
+          <li>React</li>
+          <li>Docker</li>
+          <li>Git Action- CI</li>
+          
+          </ol>    
+          
       <a>Learn React</a>
       </header>
     </div>
