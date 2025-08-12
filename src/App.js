99aa654b6c37449dadc45102531832df36cc16d0
root <root@Ramavatar.localdomain>
2025-08-12 06:02:43 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+
+      
+          Docker image of React type ho rha header
+      <a>Learn React</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
